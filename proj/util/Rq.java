b216be24dde06ea14f340d618484c5f66e474407
slyhyun <lipsul061722@jbnu.ac.kr>
2026-02-26 08:46:31 +0900

    implement Rq file

--- proj/util/Rq.java
+++ proj/util/Rq.java
@@ -1,4 +1,23 @@
 package proj.util;
 
 public class Rq {
+    private String action;
+    private int id;
+
+    public Rq(String cmd) {
+        String[] cmdBits = cmd.trim().split(" ");
+
+        this.action = cmdBits[0];
+
+        this.id = 0;
+        if (cmdBits.length > 1) {
+            try {
+                this.id = Integer.parseInt(cmdBits[1]);
+            } catch (NumberFormatException e) {
+            }
+        }
+    }
+
+    public String getAction() { return action; }
+    public int getId() { return id; }
 }
